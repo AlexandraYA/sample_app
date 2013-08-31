2145b5dab8d999eed5923c7b64e649c733d6839f
Alexandra <your.email@example.com>
2013-09-01 02:53:35 +0400

    Initial commit

---
+++ app/helpers/application_helper.rb
@@ -0,0 +1,8 @@
+module ApplicationHelper
+  Capybara.configure do |config|
+    config.match = :one
+    config.exact_options = true
+    config.ignore_hidden_elements = true
+    config.visible_text_only = true
+  end
+end

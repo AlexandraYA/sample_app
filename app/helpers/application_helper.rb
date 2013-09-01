f50dc07babdcb1bb0d6c508c71dd54408f2b251c
Alexandra <your.email@example.com>
2013-09-01 14:57:55 +0400

    Add a StaticPages controller

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,2 @@
 module ApplicationHelper
-  Capybara.configure do |config|
-    config.match = :one
-    config.exact_options = true
-    config.ignore_hidden_elements = true
-    config.visible_text_only = true
-  end
 end

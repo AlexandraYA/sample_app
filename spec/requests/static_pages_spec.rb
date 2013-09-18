1fd5477514ad9aa66a606210c9276900f6b8c946
Alexandra <your.email@example.com>
2013-09-18 13:35:54 +0400

    Add user following

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -20,20 +20,42 @@ describe "Static pages" do
 	describe "for signed-in users" do
       let(:user) { FactoryGirl.create(:user) }
       before do
-        FactoryGirl.create(:micropost, user: user, content: "Lorem ipsum")
+        30.times {FactoryGirl.create(:micropost, user: user, content: "Lorem ipsum")}
         FactoryGirl.create(:micropost, user: user, content: "Dolor sit amet")
         sign_in user
         visit root_path
       end
-
-      it "should render the user's feed" do
-        user.feed.each do |item|
-          expect(page).to have_selector("li##{item.id}", text: item.content)
+	  it { should have_selector('div.pagination') }
+	  it { should have_content("#{user.microposts.count} microposts") }
+	  
+	  it "should list each micropost" do
+        user.microposts.paginate(page: 1).each do |micropost|
+          expect(page).to have_selector('li', text: micropost.content)
         end
       end
-    end
-  end
+	  
+	  describe "pluralize" do
+		before do
+			user.microposts.each do |micropost|
+				micropost.destroy unless (user.microposts.count == 1)
+			end
+		end		
+		
+		it { should have_content("1 micropost") }
+	  end
+	  
+	  describe "follower/following counts" do
+        let(:other_user) { FactoryGirl.create(:user) }
+        before do
+          other_user.follow!(user)
+          visit root_path
+        end
 
+        it { should have_link("0 following", href: following_user_path(user)) }
+        it { should have_link("1 followers", href: followers_user_path(user)) }
+      end
+	end 
+  end
 
   describe "Help page" do
 	before { visit help_path }
